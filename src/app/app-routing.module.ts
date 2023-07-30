7e16bbc139e6343f1c42effa747ba4c9522538cc
lokesh8278 <55938652+lokesh8278@users.noreply.github.com>
2023-07-30 21:48:40 +0530

    login changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,49 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { SaleComponent } from './pages/sale/sale.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupLoginComponent } from './signup-login/signup-login.component';
+import { LoginAuthServiceService } from './authGuards/login-auth-service.service';
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    redirectTo:"login", pathMatch:"full"
+  },
+  {
+    path: "login",
+    component: SignupLoginComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    canActivate:[LoginAuthServiceService]
   },
   {
     path: 'products',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate:[LoginAuthServiceService]
   }, 
   {
     path: 'cart',
-    component: CartComponent
+    component: CartComponent,
+    canActivate:[LoginAuthServiceService]
   },
   {
     path: "sale",
-    component: SaleComponent
+    component: SaleComponent,
+    canActivate:[LoginAuthServiceService]
+  },
+  {
+    path: "navbar",
+    component: NavbarComponent,
+    canActivate:[LoginAuthServiceService]
+  },
+  {
+    path:"**",
+    redirectTo:"login",
   }
+ 
 ];
 
 @NgModule({
